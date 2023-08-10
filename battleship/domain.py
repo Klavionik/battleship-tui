771d95887bcc053654715975c88344d6e5a5e872
Roman Vlasenko <klavionik@gmail.com>
2023-08-10 07:46:19 +0300

    Implement ships

--- battleship/domain.py
+++ battleship/domain.py
@@ -11,6 +11,26 @@ class Ship:
             self.hitpoints -= 1
 
 
+class Carrier(Ship):
+    kind = "carrier"
+    hitpoints = 5
+
+
 class Battleship(Ship):
     kind = "battleship"
     hitpoints = 4
+
+
+class Cruiser(Ship):
+    kind = "cruiser"
+    hitpoints = 3
+
+
+class Submarine(Ship):
+    kind = "submarine"
+    hitpoints = 3
+
+
+class Destroyer(Ship):
+    kind = "destroyer"
+    hitpoints = 2
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -2,23 +2,40 @@ from battleship import domain
 
 
 def test_ship_hit_decreases_hitpoints():
+    carrier = domain.Carrier()
     battleship = domain.Battleship()
+    cruiser = domain.Cruiser()
+    submarine = domain.Submarine()
+    destroyer = domain.Destroyer()
 
+    carrier.hit()
     battleship.hit()
+    cruiser.hit()
+    submarine.hit()
+    destroyer.hit()
 
+    assert carrier.hitpoints == 4
     assert battleship.hitpoints == 3
+    assert cruiser.hitpoints == 2
+    assert submarine.hitpoints == 2
+    assert destroyer.hitpoints == 1
 
 
-def test_ship_hit_but_no_hp_left():
+def test_ship_not_hit_after_no_hp_left():
+    carrier = domain.Carrier()
     battleship = domain.Battleship()
+    cruiser = domain.Cruiser()
+    submarine = domain.Submarine()
+    destroyer = domain.Destroyer()
 
-    battleship.hit()
-    battleship.hit()
-    battleship.hit()
-    battleship.hit()
+    [carrier.hit() for _ in range(carrier.hitpoints + 1)]
+    [battleship.hit() for _ in range(battleship.hitpoints + 1)]
+    [cruiser.hit() for _ in range(cruiser.hitpoints + 1)]
+    [submarine.hit() for _ in range(submarine.hitpoints + 1)]
+    [destroyer.hit() for _ in range(destroyer.hitpoints + 1)]
 
+    assert carrier.hitpoints == 0
     assert battleship.hitpoints == 0
-
-    battleship.hit()
-
-    assert battleship.hitpoints == 0
+    assert cruiser.hitpoints == 0
+    assert submarine.hitpoints == 0
+    assert destroyer.hitpoints == 0
