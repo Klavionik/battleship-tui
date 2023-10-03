91237a0b1aa9e70c303ff3d9c1c5d7467c733f14
Roman Vlasenko <klavionik@gmail.com>
2023-10-03 14:37:22 +0300

    Test TargetCaller adjacent cells calling

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -27,6 +27,25 @@ def test_target_caller_doesnt_target_shot_cells():
     assert targets == [board.grid[1][0].coordinate, board.grid[1][1].coordinate]
 
 
+def test_target_caller_targets_adjacent_cells_after_hit_until_all_tried():
+    random.seed(42)
+    player_a, player_b = domain.Player("player a"), domain.Player("player b")
+    board = domain.Board()
+    ship = domain.Ship("ship", 4)
+    board.place_ship(["B2", "B3", "B4", "B5"], ship)
+    board.hit_cell("B3")
+    shot = domain.Shot(player_a, "B3", player_b, ship)
+    caller = ai.TargetCaller(board)
+
+    caller.provide_feedback([shot])  # Adds 4 adjacent cells to next targets.
+
+    assert [t.coordinate for t in caller.next_targets] == ["B2", "B4", "C3", "A3"]
+    assert caller.call_out() == ["B2"]
+    assert [t.coordinate for t in caller.next_targets] == ["B4", "C3", "A3"]
+    # When all next targets are called out, caller starts mixing in random cells.
+    assert caller.call_out(count=4) == ["B4", "C3", "A3", "C9"]
+
+
 @pytest.mark.parametrize("ship", [*domain.CLASSIC_SHIP_SUITE])
 def test_autoplacer_position_matches_ship_hp(ship):
     type_, hp = ship
