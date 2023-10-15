c9fd9d838642a02ed34b77f1b149ae8907b95ff9
Roman Vlasenko <klavionik@gmail.com>
2023-10-15 18:13:01 +0300

    Notify Board on grid leave and clean crosshair/ship preview

--- battleship/tui/widgets/board.py
+++ battleship/tui/widgets/board.py
@@ -118,6 +118,15 @@ class Cell:
         return self.render().__rich_measure__(console, options)
 
 
+class Grid(DataTable[Cell]):
+    class GridLeave(Message):
+        pass
+
+    def on_leave(self) -> None:
+        # Leave event doesn't bubble, thus we need to send a custom event
+        self.post_message(self.GridLeave())
+
+
 class Board(Widget):
     class Mode(enum.StrEnum):
         DISPLAY = "display"
@@ -141,7 +150,7 @@ class Board(Widget):
     def __init__(self, *args: Any, size: int, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.board_size = size
-        self._table = self._create_table(size)
+        self._grid = self._create_grid(size)
         self._cursor_coordinate: Coordinate | None = None
 
         self._ship_to_place: ShipToPlace | None = None
@@ -157,7 +166,7 @@ class Board(Widget):
         try:
             row, column = meta["row"], meta["column"]
         except KeyError:
-            # Event outside table.
+            # Event outside grid.
             return None
 
         if row < 0 or column < 0:
@@ -165,6 +174,11 @@ class Board(Widget):
             return None
 
         return Coordinate(row, column)
+
+    @on(Grid.GridLeave)
+    def clean_board(self) -> None:
+        self.clean_crosshair()
+        self.clean_ship_preview()
 
     @on(Mount)
     def handle_mount(self) -> None:
@@ -207,15 +221,15 @@ class Board(Widget):
         self._ship_to_place = ship
 
     def initialize_grid(self) -> None:
-        self._table.clear()
-        self._table.add_columns(*string.ascii_uppercase[: self.board_size])
+        self._grid.clear()
+        self._grid.add_columns(*string.ascii_uppercase[: self.board_size])
 
         for i, row in enumerate(range(self.board_size), start=1):
             cells = []
             for column in range(self.board_size):
                 cells.append(Cell(self.is_dark_cell((row, column))))
 
-            self._table.add_row(*cells, label=Text(str(i), style="#B0FC38 italic"))
+            self._grid.add_row(*cells, label=Text(str(i), style="#B0FC38 italic"))
 
     @staticmethod
     def is_dark_cell(coordinate: tuple[int, int]) -> bool:
@@ -233,7 +247,7 @@ class Board(Widget):
         return (row + column) % 2 == 0
 
     def compose(self) -> ComposeResult:
-        yield self._table
+        yield self._grid
 
     def move_crosshair(self, coordinate: Coordinate | None) -> None:
         self.clean_crosshair()
@@ -242,11 +256,11 @@ class Board(Widget):
             self.paint_crosshair(coordinate)
 
     def is_cell_hit(self, coordinate: Coordinate) -> bool:
-        cell = self._table.get_cell_at(coordinate)
+        cell = self._grid.get_cell_at(coordinate)
         return cell.state in (Cell.State.MISS, Cell.State.SHIP_DAMAGED, Cell.State.SHIP_DESTROYED)
 
     def is_cell_ship(self, coordinate: Coordinate) -> bool:
-        cell = self._table.get_cell_at(coordinate)
+        cell = self._grid.get_cell_at(coordinate)
         return cell.state == Cell.State.SHIP
 
     def paint_crosshair(self, coordinate: Coordinate) -> None:
@@ -257,7 +271,7 @@ class Board(Widget):
             return
 
         # Paint crosshair preserving cell's background color.
-        self._table.update_cell_at(coordinate, Cell.crosshair(self.is_dark_cell(coordinate)))
+        self._grid.update_cell_at(coordinate, Cell.crosshair(self.is_dark_cell(coordinate)))
 
     def clean_crosshair(self) -> None:
         if (
@@ -268,7 +282,7 @@ class Board(Widget):
             self.paint_empty_cell(self._cursor_coordinate)
 
     def paint_empty_cell(self, coordinate: Coordinate) -> None:
-        self._table.update_cell_at(
+        self._grid.update_cell_at(
             coordinate,
             Cell(self.is_dark_cell(coordinate)),
         )
@@ -281,7 +295,7 @@ class Board(Widget):
         self.clean_ship_preview()
 
         if coordinate:
-            self.preview_ship(coordinate.row, coordinate.column)
+            self.paint_ship_preview(coordinate.row, coordinate.column)
 
     def clean_targets(self) -> None:
         while self._target_coordinates:
@@ -308,7 +322,7 @@ class Board(Widget):
             return
 
         self._ship_to_place.rotate()  # type: ignore[union-attr]
-        self.preview_ship(*self._cursor_coordinate)  # type: ignore[misc]
+        self.move_ship_preview(self._cursor_coordinate)
 
     def clean_ship_preview(self) -> None:
         while self._preview_coordinates:
@@ -317,11 +331,11 @@ class Board(Widget):
 
     def paint_ship(self, coordinates: Iterable[Coordinate]) -> None:
         for coor in coordinates:
-            self._table.update_cell_at(coor, Cell.ship())
+            self._grid.update_cell_at(coor, Cell.ship())
 
     def paint_forbidden(self, coordinates: Iterable[Coordinate]) -> None:
         for coor in coordinates:
-            self._table.update_cell_at(coor, Cell.forbidden())
+            self._grid.update_cell_at(coor, Cell.forbidden())
 
     def is_cell_exist(self, coordinate: Coordinate) -> bool:
         return not (
@@ -329,11 +343,9 @@ class Board(Widget):
             or (coordinate.row > self.board_size - 1 or coordinate.row < 0)
         )
 
-    def preview_ship(self, row: int, column: int) -> None:
+    def paint_ship_preview(self, row: int, column: int) -> None:
         if self.mode != self.Mode.ARRANGE:
             return
-
-        self.clean_ship_preview()
 
         start = Coordinate(row, column)
 
@@ -378,15 +390,15 @@ class Board(Widget):
         self._place_forbidden = True
 
     def paint_damage(self, coordinate: Coordinate) -> None:
-        self._table.update_cell_at(coordinate, Cell.damaged(self.is_dark_cell(coordinate)))
+        self._grid.update_cell_at(coordinate, Cell.damaged(self.is_dark_cell(coordinate)))
 
     def paint_miss(self, coordinate: Coordinate) -> None:
-        self._table.update_cell_at(coordinate, Cell.miss(self.is_dark_cell(coordinate)))
+        self._grid.update_cell_at(coordinate, Cell.miss(self.is_dark_cell(coordinate)))
 
-    def _create_table(self, size: int) -> DataTable[Cell]:
+    def _create_grid(self, size: int) -> Grid:
         # 1. Disable cursor to make Click events bubble up.
         # 2. Disable cell padding to make cells square.
-        table: DataTable[Cell] = DataTable(cell_padding=0, cursor_type="none")
+        grid: Grid = Grid(cell_padding=0, cursor_type="none")
 
         # Size * cell width + row labels width.
         self.styles.width = size * 2 + 2
@@ -394,4 +406,4 @@ class Board(Widget):
         # Size + header height.
         self.styles.height = size + 1
 
-        return table
+        return grid
