1e5b6983209e8612f94483015b5f65068c76dca7
Roman Vlasenko <klavionik@gmail.com>
2023-11-12 19:15:47 +0300

    Validate login input

--- battleship/tui/screens/multiplayer.py
+++ battleship/tui/screens/multiplayer.py
@@ -5,7 +5,9 @@ from textual import on, work
 from textual.app import ComposeResult
 from textual.containers import Container, VerticalScroll
 from textual.events import Mount
+from textual.reactive import var
 from textual.screen import Screen
+from textual.validation import Length
 from textual.widgets import Button, Footer, Input, Markdown, Rule
 
 from battleship.client import Client, RequestFailed
@@ -14,6 +16,9 @@ from battleship.tui import resources, screens
 
 class Multiplayer(Screen[None]):
     BINDINGS = [("escape", "back", "Back")]
+    is_nickname_valid: var[bool] = var(False)
+    is_password_valid: var[bool] = var(False)
+    is_input_valid: var[bool] = var(False)
 
     @inject.param("client", Client)
     def __init__(self, *args: Any, client: Client, **kwargs: Any) -> None:
@@ -23,18 +28,24 @@ class Multiplayer(Screen[None]):
         with resources.get_resource("multiplayer_help.md").open() as fh:
             self.help = fh.read()
 
-        self._nickname = ""
-        self._password = ""
-
     def compose(self) -> ComposeResult:
         with Container(classes="main"):
             with VerticalScroll():
                 yield Markdown(self.help, classes="screen-help")
 
             with Container(classes="screen-content"):
-                yield Input(placeholder="Nickname", id="nickname")
-                yield Input(placeholder="Password", id="password", password=True)
-                yield Button("Connect", variant="primary", id="connect-user")
+                yield Input(
+                    placeholder="Nickname",
+                    id="nickname",
+                    validators=[Length(minimum=7, maximum=20)],
+                )
+                yield Input(
+                    placeholder="Password",
+                    id="password",
+                    password=True,
+                    validators=[Length(minimum=9)],
+                )
+                yield Button("Connect", variant="primary", id="connect-user", disabled=True)
                 yield Rule(line_style="heavy")
                 yield Button("Connect as guest", id="connect-guest")
 
@@ -47,20 +58,37 @@ class Multiplayer(Screen[None]):
     def on_mount(self) -> None:
         self.query_one("#nickname", Input).focus()
 
-    @on(Input.Changed, "#nickname")
-    def update_nickname(self, event: Input.Changed) -> None:
-        self._nickname = event.value
+    def compute_is_input_valid(self) -> bool:
+        return self.is_password_valid and self.is_nickname_valid
 
-    @on(Input.Changed, "#password")
-    def update_password(self, event: Input.Changed) -> None:
-        self._password = event.value
+    def watch_is_input_valid(self, old_valid: bool, valid: bool) -> None:
+        if old_valid == valid:
+            return
+
+        self.query_one("#connect-user", Button).disabled = not valid
+
+    @on(Input.Changed)
+    def validate_nickname(self, event: Input.Changed) -> None:
+        validation = event.validation_result
+
+        if validation is not None:
+            if event.input.id == "nickname":
+                self.is_nickname_valid = validation.is_valid  # noqa
+
+            if event.input.id == "password":
+                self.is_password_valid = validation.is_valid  # noqa
+
+    @on(Input.Submitted)
+    def submit(self) -> None:
+        if self.is_input_valid:
+            self.login()
 
     @on(Button.Pressed, "#connect-guest")
-    async def connect_as_guest(self) -> None:
+    def connect_as_guest(self) -> None:
         self.login(guest=True)
 
     @on(Button.Pressed, "#connect-user")
-    async def connect_as_user(self) -> None:
+    def connect_as_user(self) -> None:
         self.login()
 
     @work
@@ -71,7 +99,9 @@ class Multiplayer(Screen[None]):
             if guest:
                 nickname = await self._client.login(guest=True)
             else:
-                nickname = await self._client.login(self._nickname, self._password)
+                nickname = self.query_one("#nickname", Input).value
+                password = self.query_one("#password", Input).value
+                await self._client.login(nickname, password)
         except RequestFailed:
             self.loading = False  # noqa
             self.notify(
