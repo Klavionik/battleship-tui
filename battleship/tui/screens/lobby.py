48d1475f89a60b3b1ae390d5dd2ed8f6475b6e28
Roman Vlasenko <klavionik@gmail.com>
2023-11-13 20:50:26 +0300

    Focus Lobby menu on mount

--- battleship/tui/screens/lobby.py
+++ battleship/tui/screens/lobby.py
@@ -4,7 +4,7 @@ import inject
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Container, VerticalScroll
-from textual.events import Unmount
+from textual.events import Mount, Unmount
 from textual.screen import Screen
 from textual.widgets import Footer, Label, ListItem, ListView, Markdown, Static
 
@@ -42,6 +42,10 @@ class Lobby(Screen[None]):
 
     def action_back(self) -> None:
         self.app.switch_screen(screens.MainMenu())
+
+    @on(Mount)
+    def focus_menu(self) -> None:
+        self.query_one(ListView).focus()
 
     @on(Unmount)
     async def disconnect_ws(self) -> None:
