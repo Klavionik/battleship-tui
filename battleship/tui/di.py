5eb7bee96127bfe9f44e6635002bb5578fc8adee
Roman Vlasenko <klavionik@gmail.com>
2024-08-01 00:16:10 +0300

    Fix settings/credentials provider injection

--- battleship/tui/di.py
+++ battleship/tui/di.py
@@ -9,8 +9,8 @@ container = Container()
 
 
 def configure(config: Config) -> None:
-    container.add_singleton(CredentialsProvider, config.credentials_provider)
-    container.add_singleton(SettingsProvider, config.game_settings_provider)
+    container.add_instance(config.credentials_provider(), CredentialsProvider)
+    container.add_instance(config.game_settings_provider(), SettingsProvider)
     container.add_instance(
         Client(str(config.server_url), container.resolve(CredentialsProvider)), Client
     )
